3693ffdf23103688131006b11641e3f700140db7
Leonel <balbino.leonel@gmail.com>
2021-02-02 23:11:58 +0000

    migracao do banco de dados

--- gestao_usuarios/models.py
+++ gestao_usuarios/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class ArrobaModel(models.Model):
     arroba = models.CharField(max_length=100)
+    last_query_date = models.DateField(default=datetime.now, blank=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
--- setup/settings.py
+++ setup/settings.py
@@ -79,10 +79,25 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'twitter-monitor',
+        'USER': os.environ.get('MYSQL_TWITTER_USER'),
+        'PASSWORD': os.environ.get('MYSQL_TWITTER_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_TWITTER_HOST'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
